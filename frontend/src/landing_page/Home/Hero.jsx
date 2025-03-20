76c6bcad89c7398ea744f29d1ae15ac75c675f1e
Pritam-kumar-03 <kpritam0307@gmail.com>
2025-03-20 12:59:47 +0530

    homepage Navbar completed

--- frontend/src/landing_page/Home/Hero.jsx
+++ frontend/src/landing_page/Home/Hero.jsx
@@ -1,7 +1,53 @@
+import "./Hero.css";
+
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className="container-fluid hero">
+      <div className="row hero-row">
+        <div className="col-5 hero-first-col">
+          <div>
+            <h6>FURNITURE DESIGNS IDEA</h6>
+          </div>
+          <div>
+            <h2>
+              Modern Interior <br></br>
+              Design Studio
+            </h2>
+          </div>
+          <div>
+            <p>
+              Choosing the right furniture for your home online will add{" "}
+              <br></br>elegance and functionality to your interior while{" "}
+              <br></br>it will also be cost-effective and long-lasting.
+            </p>
+          </div>
+          <div>
+            <span>
+              <button>
+                Shop NOW<i class="fa-solid fa-arrow-right"></i>{" "}
+              </button>
+            </span>
+            <span>Follow Instagram</span>
+          </div>
+          <div>
+            <span>
+              2500+
+              Unique Styles
+            </span>
+            <span>
+              5000+
+              Happy Clients
+            </span>
+            <span>
+                300+
+                Cerified Outlets
+            </span>
+          </div>
+        </div>
+        <div className="col-7 hero-second-col">2</div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- frontend/src/landing_page/Home/Navbar.jsx
+++ frontend/src/landing_page/Home/Navbar.jsx
@@ -1,7 +1,58 @@
+import "./Navbar.css"
+
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-     );
+  return (
+    <div className="container-fluid Navbar">
+      <div className="row Navbar_row ">
+        <div className="col-3 nav-first-col ">
+          <span> 
+            <h1  className="Nav-heading">FurniFlex</h1>
+          </span>
+        </div>
+        <div className="col-6 nav-second-col  ">
+          <div className="nav-second-col-div pt-2">
+            <ul className="nav-second-col-ul">
+              <li className="nav-links">
+                {" "}
+                <a href="">Home</a>
+              </li>
+              <li className="nav-links">
+                {" "}
+                <a href="">Products</a>
+              </li>
+              <li className="nav-links">
+                {" "}
+                <a href="">About us</a>
+              </li>
+              <li className="nav-links">
+                {" "}
+                <a href=""> Contact us</a>
+              </li>
+              <li className="nav-links">
+                {" "}
+                <a href="">Blog</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-3 nav-third-col">
+          <span className="nav-icons">
+            <i class="fa-solid fa-search"></i>
+            
+          </span>
+          <span className="nav-icons">
+            <i class="fa-regular fa-heart"></i>
+          </span>
+          <span className="nav-icons">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </span>
+          <span className="nav-icons">
+            <i class="fa-regular fa-user"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
