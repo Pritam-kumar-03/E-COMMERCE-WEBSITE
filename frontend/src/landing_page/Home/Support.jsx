eea3c0f24df78cac2114d2a56f1fecd845fa2871
Pritam-kumar-03 <kpritam0307@gmail.com>
2025-03-22 13:00:19 +0530

    support half complete

--- frontend/src/landing_page/Home/Support.jsx
+++ frontend/src/landing_page/Home/Support.jsx
@@ -1,7 +1,47 @@
+import "./Support.css";
+
 function Support() {
-    return ( 
-        <h1>Support</h1>
-     );
+  return (
+    <div className="conatiner mt-5">
+      <div className="row">
+        <div className="col-3">
+          <span className="support-icon1">
+            <i class="fa-solid fa-truck"></i>
+          </span><br></br>
+          <span>
+            Fast & Free <br /> Shipping
+          </span>
+        </div>
+        <div className="col-3">
+          <span className="support-icon2">
+            <i class="fa-solid fa-bag-shopping"></i>
+          </span><br></br>
+          <span>
+            Easy to <br />
+            Shop
+          </span>
+        </div>
+        <div className="col-3">
+          <span className="support-icon3">
+            <i class="fa-solid fa-phone-volume"></i>
+          </span><br></br>
+          <span>
+            24/7 <br />
+            Support
+          </span>
+        </div>
+        <div className="col-3">
+          <span className="support-icon3">
+            <i class="fa-solid fa-arrows-rotate"></i>
+          </span><br></br>
+          <span>
+            Hassle Free <br />
+            Returns
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Support;
