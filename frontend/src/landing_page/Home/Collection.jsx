0d5af9727700976e724742df6a44f4141cd6176c
Pritam-kumar-03 <kpritam0307@gmail.com>
2025-03-22 21:09:33 +0530

    section half done

--- frontend/src/landing_page/Home/Collection.jsx
+++ frontend/src/landing_page/Home/Collection.jsx
@@ -1,7 +1,70 @@
+import "./collection.css";
+
 function Collection() {
-    return ( 
-        <h1>Collection</h1>
-     );
+  return (
+    <div className="container collection-container mt-5">
+      <div className="row collection-row">
+        <div className="col-7 row-first-col">
+          <div className="row first-col-firstrow">
+            <div className="col-12 center-table">
+              <div className=" center-table-row">
+                <div className=" center-table-des">
+                  <p style={{borderR:""}}>NEW COLLECTION</p>
+                  <h5>Center Table</h5>
+                  <ul>
+                    <li>Square table</li>
+                    <li>Round table</li>
+                    <li>Wooden table</li>
+                    <li>Glass table</li>
+                  </ul>
+                  <a href="">
+                    View All <i class="fa-solid fa-arrow-right"></i>
+                  </a>
+                  <div className=" center-table-img"></div>
+                </div>
+
+               
+              </div>
+            </div>
+          </div>
+          <div className="row first-col-secondrow">
+            <div className="col-6 lighting-lamp">
+              <p className="col-7 lighting-lamp-des">
+                {" "}
+                <p>NEW COLLECTION</p>
+                <h5>Lighting Lamp</h5>
+                <ul>
+                  <li>Flore lamps</li>
+                  <li>Tripod lamps</li>
+                  <li>Table lamps</li>
+                  <li>Study lamps</li>
+                </ul>
+                <a href="">
+                  View All <i class="fa-solid fa-arrow-right"></i>
+                </a>
+              </p>
+              <div className="col-5 lighting-lamp-img"></div>
+            </div>
+            <div className="col-6 discount20">4</div>
+          </div>
+        </div>
+        <div className="col-5 row-second-col">
+          <p className="ancent-chair-des">
+          <p>NEW COLLECTION</p>
+                   <h5>Accent Chairs</h5>
+                   <ul>
+                    <li>Arm Chair</li>
+                    <li>Wing Chair</li>
+                    <li>Cafe Chair</li>
+                    <li>Wheels Chair</li>
+                   </ul>
+                  <a href="">View All <i class="fa-solid fa-arrow-right"></i></a>
+          </p>
+          <div className="accentchair"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Collection;
--- frontend/src/landing_page/Home/Support.jsx
+++ frontend/src/landing_page/Home/Support.jsx
@@ -2,42 +2,42 @@ import "./Support.css";
 
 function Support() {
   return (
-    <div className="conatiner mt-5">
-      <div className="row">
-        <div className="col-3">
+    <div className="container mt-5 support-container ">
+      <div className="row support-row mb-5">
+        <div className="col-3 support-first-col">
           <span className="support-icon1">
             <i class="fa-solid fa-truck"></i>
           </span><br></br>
-          <span>
+          <div className="suppot-state">
             Fast & Free <br /> Shipping
-          </span>
+          </div>
         </div>
         <div className="col-3">
           <span className="support-icon2">
             <i class="fa-solid fa-bag-shopping"></i>
           </span><br></br>
-          <span>
+          <div className="suppot-state">
             Easy to <br />
             Shop
-          </span>
+          </div>
         </div>
         <div className="col-3">
           <span className="support-icon3">
             <i class="fa-solid fa-phone-volume"></i>
           </span><br></br>
-          <span>
+          <div className="suppot-state">
             24/7 <br />
             Support
-          </span>
+          </div>
         </div>
         <div className="col-3">
           <span className="support-icon3">
             <i class="fa-solid fa-arrows-rotate"></i>
           </span><br></br>
-          <span>
+          <div className="suppot-state">
             Hassle Free <br />
             Returns
-          </span>
+          </div>
         </div>
       </div>
     </div>
